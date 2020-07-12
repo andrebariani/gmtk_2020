bc7407e2f7cfe9f927469fed7e7cebe5a88eb645
andrebariani <andrebariani@gmail.com>
2020-07-12 15:20:31 -0300

    sons de fuel e star

--- Scenes/Item/Fuel.gd
+++ Scenes/Item/Fuel.gd
@@ -4,4 +4,12 @@ func _on_Area2D_area_entered(area):
 	if area.has_method("gotten_fuel"):
 		area.gotten_fuel()
 	
+		$AudioStreamPlayer.play(0)
+		$Area2D/CollisionShape2D.disabled = true
+		self.visible = false
+	else:
+		destroy()
+
+
+func _on_AudioStreamPlayer_finished():
 	destroy()
--- Scenes/Item/Star.gd
+++ Scenes/Item/Star.gd
@@ -3,5 +3,13 @@ extends "res://Scenes/Item/Item.gd"
 func _on_Area2D_area_entered(area):
 	if area.has_method("score"):
 		area.score()
-	
+		
+		$AudioStreamPlayer.play(0)
+		$Area2D/CollisionShape2D.disabled = true
+		self.visible = false
+	else:
+		destroy()
+
+
+func _on_AudioStreamPlayer_finished():
 	destroy()
